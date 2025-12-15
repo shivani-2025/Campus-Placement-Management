5bb8ec6cd1eb060349456f3c07a18dacf57268fe
VinugnaK <kodurilakshmivinugna@gmail.com>
2025-12-15 10:32:18 +0530

    Revert "added slot,online slot and tiertotal round entities"
    This reverts commit afd3ac0ddef2969c7006e32ef7c31889982f30f3.

--- src/main/java/com/application/TierTotalRound/TierTotalRoundRepository.java
+++
@@ -1,7 +0,0 @@
-package com.application.TierTotalRound;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TierTotalRoundRepository extends JpaRepository<TierTotalRound, Integer> {
-}
--- src/main/java/com/application/slot/SlotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.slot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SlotRepository extends JpaRepository<Slot, Integer> {
-
-}
